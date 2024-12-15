3ea3a3f077c5ac453fb52be8c7aca2de55d044bc
kbundi <kbundi90@gmail.com>
2024-12-15 09:49:53 -0800

    created python_introduction/basic_operations.py and .py code done

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,18 @@
+#!/bin/bash
+number1 = 10
+number2 = 5
+
+# Calculate sum
+sum = number1 + number2
+
+# Calculate difference
+difference = number1 - number2
+
+# Calculate product
+product = number1 * number2
+
+# Print the results
+print("Sum:", sum)
+print("Difference:", difference)
+print("Product:", product)
+
