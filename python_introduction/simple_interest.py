ef421131193d8c557f4d202ae9cf220451467c14
kbundi <kbundi90@gmail.com>
2024-12-15 10:22:50 -0800

    created python_introduction/simple_interest.py and respective .py code

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Simple Interest Calculation
+
+principal = 1000  # Initial investment (in dollars)
+rate = 0.05  # Annual interest rate (as a decimal)
+time = 3  # Investment period (in years)
+
+# Calculate simple interest
+interest = principal * rate * time
+
+# Print the result
+print("The simple interest is:", interest)
+
