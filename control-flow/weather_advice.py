73bedbf4507b7b0bda7bfd3782d593015a010831
kbundi <kbundi90@gmail.com>
2024-12-20 03:27:51 -0800

    editted weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,13 +1,10 @@
 #!bin/bash
 weather = str(input("What's the weather like today? (sunny/rainy/cold): "))
-valid_weather = ["sunny", "rainy", "cold"]
-
-if weather in valid_weather:
-    if weather == sunny:
-        print ("Wear a t-shirt and sunglasses.")
+if weather == sunny:
+    print ("Wear a t-shirt and sunglasses.")
 elif weather == rainy:
     print ("Don't forget your umbrella and a raincoat.")
 elif weather == cold:
-     print ("Make sure to wear a warm coat and a scarf.")
+    print ("Make sure to wear a warm coat and a scarf.")
 else:
     print ("Sorry, I don't have recommendations for this weather..")
