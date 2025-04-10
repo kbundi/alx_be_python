dca0afba0cd464a3ad63b4c6c0a01f67949f6663
kbundi <kbundi90@gmail.com>
2025-04-09 23:40:56 -0700

    division by zero sorted

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -22,7 +22,9 @@ elif operation == 'multiply':
     return num1*num2
 
 elif operation == 'divide':
-    if num2 == '0':
-        return "Error Divieion by Zero"
+    if num2 == 0:
+        return "Error Division by Zero"
     else: return num1/num2
+else:
+    return "Error Invaid Operations"
 
